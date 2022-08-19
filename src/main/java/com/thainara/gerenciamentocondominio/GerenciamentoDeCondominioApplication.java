5090194a53a1956acb8882e6ddce6494e130e6a7
Thainara Carvalho <thai.cds1@gmail.com>
2022-08-19 13:08:09 -0300

    gerênciamento de despesas do condomínio, inicializando projeto

---
+++ src/main/java/com/thainara/gerenciamentocondominio/GerenciamentoDeCondominioApplication.java
@@ -0,0 +1,13 @@
+package com.thainara.gerenciamentocondominio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeCondominioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciamentoDeCondominioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thainara/gerenciamentocondominio/GerenciamentoDeCondominioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thainara.gerenciamentocondominio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeCondominioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
